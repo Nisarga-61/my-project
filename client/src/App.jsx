4f9ce4d4097d6ede9152833cc78da3d9803a7724
Gaganaks <shekhargagana2@gmail.com>
2026-04-23 15:39:57 +0530

    1234

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ResetPassword from './Pages/Login';
+import Login from './Pages/Login';
+// import Login from './Pages/Login'; // Uncomment this to view the Login page
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* <Login /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Login.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import '../Styles/Login.css';
+
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="login-card">
+        <div className="login-header">
+          <div className="login-icon">
+            {/* Bus SVG Icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+              />
+            </svg>
+          </div>
+          <h1 className="login-title">Welcome Back</h1>
+          <p className="login-subtitle">Sign in to book your next journey</p>
+        </div>
+
+        <form className="login-form" onSubmit={(e) => e.preventDefault()}>
+          <div className="input-group">
+            <label htmlFor="email">Email / Username</label>
+            <div className="input-wrapper">
+              <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+              </svg>
+              <input
+                type="text"
+                id="email"
+                className="input-field"
+                placeholder="Enter your email or username"
+              />
+            </div>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <div className="input-wrapper">
+              <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+              </svg>
+              <input
+                type="password"
+                id="password"
+                className="input-field"
+                placeholder="Enter your password"
+              />
+            </div>
+          </div>
+
+          <div className="forgot-password">
+            <a href="#" onClick={(e) => e.preventDefault()}>Forgot password?</a>
+          </div>
+
+          <button type="button" className="login-btn" onClick={(e) => e.preventDefault()}>
+            Sign In
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" style={{ width: '18px', height: '18px' }}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+            </svg>
+          </button>
+        </form>
+
+        <div className="login-footer">
+          Don't have an account? <a href="#" onClick={(e) => e.preventDefault()}>Sign up</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
